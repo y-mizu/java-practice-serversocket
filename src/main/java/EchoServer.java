2c6a8f7caeeacd709cc19b3382eec734ac9a2dff
y-mizu <yuichiro.mizukami@topgate.co.jp>
2017-06-15 12:08:09 +0900

    コードレビューの指摘を元に変数を整理したり、データの読み取り方を変えたり

--- src/main/java/EchoServer.java
+++ src/main/java/EchoServer.java
@@ -4,55 +4,46 @@ import java.net.Socket;
 
 // １ EchoServerクラスをつくる
 public class EchoServer {
+    public static final int PORT = 8080;
     public static void main(String[] args) {
-        Socket socket;
-        int PORT = 8080;
-
-        //２ サーバーソケットのインスタンスを生成する
+        // ２ ServerSocket型の変数serverSocketを初期化　
         ServerSocket serverSocket = null;
-        try {
-            serverSocket = new ServerSocket(PORT);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
-        //<繰返>
         while (true) {
             try {
-                //３ サーバーソケットに対する接続要求を待機して、それを受け取る(accept)
-                socket = serverSocket.accept();
+                //３ serverSocketのインスタンスを生成　
+                serverSocket = new ServerSocket(PORT);
+                //４ サーバーソケットに対する接続要求を待機して、それを受け取る(accept)
+                Socket socket = serverSocket.accept();
 
-                //４ InputStream型の変数isに通信ソケットから受信するバイトストリームを取得
+                //５ InputStream型の変数isに通信ソケットから受信するバイトストリームを取得
                 InputStream is = socket.getInputStream();
 
-                //５ OutputStream型の変数osに通信ソケットに送信するバイトストリームを取得
+                //６ OutputStream型の変数osに通信ソケットに送信するバイトストリームを取得
                 OutputStream os = socket.getOutputStream();
 
-                //６ DataInputStreamのインスタンスの生成及びPrintStream型のインスタンスの生成
-                DataInputStream dis = new DataInputStream(is);
+                //７ BufferedReaderのインスタンスの生成及びPrintStream型のインスタンスの生成
+                BufferedReader br = new BufferedReader(new InputStreamReader(is));
                 PrintStream ps = new PrintStream(os);
 
-                //７ クライアントから文字列を受信し、標準出力に出力する
-                String receive = dis.readLine();
+                //８ クライアントから文字列を受信し、サーバーの標準出力に出力する
+                String receive = br.readLine();
                 System.out.println(receive + "が入力されました。");
 
-                //８ クライアント返すために、変数sendに標準出力receiveを代入
-                String send = "Received:" +receive;
-
                 //９ クライアントにメッセージを送る
-                ps.println(send);
+                ps.println(receive);
 
                 //１０　ストリームやソケットを閉じる
-                dis.close();
-                ps.close();
+                socket.close();
+                serverSocket.close();
                 is.close();
                 os.close();
-                socket.close();
+                br.close();
+                ps.close();
 
             } catch (IOException e1) {
                 e1.printStackTrace();
             }
-
         }
     }
 }
