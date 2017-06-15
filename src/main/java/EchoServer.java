8f2cedf635387d479199cb49fce12f53962fc201
y-mizu <yuichiro.mizukami@topgate.co.jp>
2017-06-15 18:15:21 +0900

    コードレビューの指摘を元にSockeとserverSocketのcloseをfinallyで閉じたりした

--- src/main/java/EchoServer.java
+++ src/main/java/EchoServer.java
@@ -5,43 +5,61 @@ import java.net.Socket;
 // 1 EchoServerクラスをつくる
 public class EchoServer {
     public static final int PORT = 8080;
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) {
+        //2 ServerSocketとSocketを初期化
+        ServerSocket serverSocket = null;
+        Socket socket = null;
 
         try {
-            //2 ServerSocketのインスタンスを生成　
-            ServerSocket serverSocket = new ServerSocket(PORT);
+            //3 ServerSocketのインスタンスを生成　
+            serverSocket = new ServerSocket(PORT);
 
             while (true) {
-                //3 サーバーソケットに対する接続要求を待機して、それを受け取る(accept)
-                Socket socket = serverSocket.accept();
+                //4 サーバーソケットに対する接続要求を待機して、それを受け取る(accept)
+                socket = serverSocket.accept();
 
-                //4 InputStream型のインスタンスisに通信ソケットから受信するバイトストリームを取得
+                //5 InputStream型のインスタンスisに通信ソケットから受信するバイトストリームを取得
                 InputStream is = socket.getInputStream();
 
-                //5 OutputStream型のosに通信ソケットに送信するバイトストリームを取得
+                //6 OutputStream型のインスタンスosに通信ソケットに送信するバイトストリームを取得
                 OutputStream os = socket.getOutputStream();
 
-                //6 BufferedReaderのインスタンスの生成及びPrintStream型のインスタンスの生成
+                //7 BufferedReaderのインスタンスの生成及びPrintStream型のインスタンスの生成
                 BufferedReader br = new BufferedReader(new InputStreamReader(is));
                 PrintStream ps = new PrintStream(os);
 
-                //7 クライアントから文字列を受信し、サーバーの標準出力に出力する
+                //8 クライアントから文字列を受信し、サーバーの標準出力に出力する
                 String receive = br.readLine();
                 System.out.println(receive + "が入力されました。");
 
-                //8 クライアントにメッセージを送る
+                //9 クライアントにメッセージを送る
                 ps.println(receive);
 
-                //9　ストリームやソケットを閉じる
-                serverSocket.close();
-                socket.close();
+                //10　ストリームを閉じる
                 is.close();
                 os.close();
                 br.close();
                 ps.close();
+
             }
-        } catch (IOException e){
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            //11 例外処理とか
+            try {
+                if (serverSocket != null)
+                    serverSocket.close();
+            } catch (IOException e) {
                 e.printStackTrace();
+            }
+            try {
+                if (socket != null)
+                    socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 }
