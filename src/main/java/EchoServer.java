f1d93dc843b468e986e695cecdc92d02ee5d51bd
y-mizu <yuichiro.mizukami@topgate.co.jp>
2017-06-15 16:51:01 +0900

    コードレビューの指摘を元にServerSocketの位置を変えたり、tryとwhileの入れ子を組み替えたり

--- src/main/java/EchoServer.java
+++ src/main/java/EchoServer.java
@@ -2,48 +2,46 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-// １ EchoServerクラスをつくる
+// 1 EchoServerクラスをつくる
 public class EchoServer {
     public static final int PORT = 8080;
     public static void main(String[] args) throws IOException {
-        // ２ ServerSocket型の変数serverSocketを初期化　
-        ServerSocket serverSocket = null;
 
-        while (true) {
-            try {
-                //３ serverSocketのインスタンスを生成　
-                serverSocket = new ServerSocket(PORT);
-                //４ サーバーソケットに対する接続要求を待機して、それを受け取る(accept)
+        try {
+            //2 ServerSocketのインスタンスを生成　
+            ServerSocket serverSocket = new ServerSocket(PORT);
+
+            while (true) {
+                //3 サーバーソケットに対する接続要求を待機して、それを受け取る(accept)
                 Socket socket = serverSocket.accept();
 
-                //５ InputStream型の変数isに通信ソケットから受信するバイトストリームを取得
+                //4 InputStream型のインスタンスisに通信ソケットから受信するバイトストリームを取得
                 InputStream is = socket.getInputStream();
 
-                //６ OutputStream型の変数osに通信ソケットに送信するバイトストリームを取得
+                //5 OutputStream型のosに通信ソケットに送信するバイトストリームを取得
                 OutputStream os = socket.getOutputStream();
 
-                //７ BufferedReaderのインスタンスの生成及びPrintStream型のインスタンスの生成
+                //6 BufferedReaderのインスタンスの生成及びPrintStream型のインスタンスの生成
                 BufferedReader br = new BufferedReader(new InputStreamReader(is));
                 PrintStream ps = new PrintStream(os);
 
-                //８ クライアントから文字列を受信し、サーバーの標準出力に出力する
+                //7 クライアントから文字列を受信し、サーバーの標準出力に出力する
                 String receive = br.readLine();
                 System.out.println(receive + "が入力されました。");
 
-                //９ クライアントにメッセージを送る
+                //8 クライアントにメッセージを送る
                 ps.println(receive);
 
-                //１０　ストリームやソケットを閉じる
-                socket.close();
+                //9　ストリームやソケットを閉じる
                 serverSocket.close();
+                socket.close();
                 is.close();
                 os.close();
                 br.close();
                 ps.close();
-
-            } finally {
-                if(serverSocket != null) serverSocket.close();
             }
+        } catch (IOException e){
+                e.printStackTrace();
         }
     }
 }
