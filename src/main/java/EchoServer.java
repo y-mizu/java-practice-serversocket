9da350144e7e5fda020d67ae7a687f4c40d1328e
y-mizu <yuichiro.mizukami@topgate.co.jp>
2017-06-15 19:07:35 +0900

    最後のCode Reviewを元にtry-catchを正しい位置にしたり、socketとServersocketの範囲にあった位置に移動した

--- src/main/java/EchoServer.java
+++ src/main/java/EchoServer.java
@@ -7,59 +7,70 @@ public class EchoServer {
     public static final int PORT = 8080;
 
     public static void main(String[] args) {
-        //2 ServerSocketとSocketを初期化
+        //2 ServerSocketを初期化
         ServerSocket serverSocket = null;
-        Socket socket = null;
 
         try {
             //3 ServerSocketのインスタンスを生成　
             serverSocket = new ServerSocket(PORT);
 
             while (true) {
-                //4 サーバーソケットに対する接続要求を待機して、それを受け取る(accept)
-                socket = serverSocket.accept();
+                //4 Socketを初期化
+                Socket socket = null;
+                try {
+                    //5 サーバーソケットに対する接続要求を待機して、それを受け取る
+                    socket = serverSocket.accept();
 
-                //5 InputStream型のインスタンスisに通信ソケットから受信するバイトストリームを取得
-                InputStream is = socket.getInputStream();
+                    //5 InputStream型のインスタンスisに通信ソケットから受信するバイトストリームを取得
+                    InputStream is = socket.getInputStream();
 
-                //6 OutputStream型のインスタンスosに通信ソケットに送信するバイトストリームを取得
-                OutputStream os = socket.getOutputStream();
+                    //6 OutputStream型のインスタンスosに通信ソケットに送信するバイトストリームを取得
+                    OutputStream os = socket.getOutputStream();
 
-                //7 BufferedReaderのインスタンスの生成及びPrintStream型のインスタンスの生成
-                BufferedReader br = new BufferedReader(new InputStreamReader(is));
-                PrintStream ps = new PrintStream(os);
+                    //7 BufferedReaderのインスタンスの生成及びPrintStream型のインスタンスの生成
+                    BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                    PrintStream ps = new PrintStream(os);
 
-                //8 クライアントから文字列を受信し、サーバーの標準出力に出力する
-                String receive = br.readLine();
-                System.out.println(receive + "が入力されました。");
+                    //8 クライアントから文字列を受信し、サーバーの標準出力に出力する
+                    String receive = br.readLine();
+                    System.out.println(receive + "が入力されました。");
 
-                //9 クライアントにメッセージを送る
-                ps.println(receive);
+                    //9 クライアントにメッセージを送る
+                    ps.println(receive);
 
-                //10　ストリームを閉じる
-                is.close();
-                os.close();
-                br.close();
-                ps.close();
+                    //10　ストリームを閉じる
+                    // is.close(); 大枠にあるbrがcloseされると閉じる
+                    // os.close(); 大枠にあるbrがcloseされると閉じる
+                    br.close();
+                    ps.close();
 
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    // 次のクライアントへ
+                } finally {
+                    try {
+                        if (socket != null) {
+                            socket.close();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
+            // serverSocketの例外処理によって終了させる
         } catch (IOException e) {
             e.printStackTrace();
+            System.exit(1);
+            // serverSocketがうまく動かなかった時に閉じる処理
         } finally {
-            //11 例外処理とか
             try {
-                if (serverSocket != null)
+                if (serverSocket != null) {
                     serverSocket.close();
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            try {
-                if (socket != null)
-                    socket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
         }
+
     }
 }
